bbe659f3e3fc45928cb3be8fc524c98ded2f2d75
NOSI / BIS  - Gerson de Oliveira Monteiro <gerson.o.monteiro@nosi.cv>
2024-05-10 17:04:30 -0100

    update env

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,7 +2,8 @@ export const STRAPI_API = `${process.env.STRAPI_HOST}/api/${process.env.STRAPI_C
 export const NDC_API_IMAGE =
   process.env.API_IMAGE + "/v1/Certificate/getImageCertificate";
 
-// TODO: Thirdweb api key client id
-export const NEXT_PUBLIC_TEMPLATE_CLIENT_ID = "TODO";
-// TODO:
-export const NEXT_PUBLIC_NFT_COLLECTION_CONTRACT_ADDRESS = "TODO";
+export const NEXT_PUBLIC_TEMPLATE_CLIENT_ID =
+  "3d6f5fac45847e76612f1e4d2174400f";
+
+export const NEXT_PUBLIC_NFT_COLLECTION_CONTRACT_ADDRESS =
+  "0x9D10986d461eDb3DaFD57A3885390bFfeDA435FA";
